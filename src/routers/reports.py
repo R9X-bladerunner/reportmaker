9c62d385f496d9b00f8a549b20e158c23c488561
air1992 <dndrv12345@gmail.com>
2023-08-17 04:31:26 +0800

    v01-3 refactor report route( replace background task to ReportService, add report name generating by template.title

--- src/routers/reports.py
+++ src/routers/reports.py
@@ -1,24 +1,20 @@
-import os
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 from fastapi.responses import FileResponse
-from starlette.background import BackgroundTasks
+
 
 from src.services.reports import ReportService
 
 report_router = APIRouter(prefix="/reports", tags=["Reports"])
 
-# Функция удаления временного файла отчета
-def remove_file(path: str) -> None:
-    os.unlink(path)
+
 
 @report_router.get("/{template_id}/patients/{patient_id}")
 async def get_report(
                 template_id: int,
                 patient_id: int,
                 background_tasks: BackgroundTasks,
-                service: ReportService = Depends()):
+                service: ReportService = Depends()) -> FileResponse:
 
-        report_file_path = await service.get_report(template_id, patient_id)
-        background_tasks.add_task(remove_file, report_file_path)
-        return FileResponse(report_file_path, filename='report.pdf', media_type='application/pdf')
+        report_file_responce = await service.get_report(template_id, patient_id, background_tasks)
+        return report_file_responce
--- src/services/reports.py
+++ src/services/reports.py
@@ -3,7 +3,8 @@ import pdfkit
 import jinja2
 import tempfile
 
-from fastapi import Depends
+from fastapi import Depends, BackgroundTasks
+from fastapi.responses import FileResponse
 from pydantic import parse_obj_as
 
 
@@ -12,7 +13,9 @@ from src.services.patients import PatientService
 from src.services.templates import TemplateService
 
 
-
+# Функция удаления временного файла отчета
+def remove_file(path: str) -> None:
+    os.unlink(path)
 
 
 class ReportService:
@@ -31,7 +34,7 @@ class ReportService:
         return parsed_template
 
 
-    async def get_report(self, template_id: int, patient_id: int):
+    async def get_report(self, template_id: int, patient_id: int, background_task: BackgroundTasks):
         # Получение данных из БД
         template = await self.template_service.get_template_by_id(template_id)
         patient = await self.patient_service.get_patient_by_id(patient_id)
@@ -45,8 +48,11 @@ class ReportService:
             temp_path = temp_pdf.name
             pdfkit.from_string(parsed_template, temp_path, options={'encoding': 'utf-8'})
 
+        #Добавление background task для удаления
+        background_task.add_task(remove_file, temp_path)
+
         # Возврат временного пути к файлу отчету
-        return temp_path
+        return FileResponse(temp_path, filename=f'{template.title}.pdf', media_type='application/pdf')
 
 
 
